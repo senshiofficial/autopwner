a5bbe7ec76899aefe5af523584428a0fecef39b4
senshiofficial <senshi_official@proton.me>
2023-11-03 03:05:42 +0000

    ANSI instead Colorama

--- intro.py
+++ intro.py
@@ -1,8 +1,8 @@
-from colorama import Fore
 import time
-def intro(text, delay=0.003):
-    print(Fore.RED, end='')
+def intro(args, text, delay=0.003):
+    color = 35
+    if args != None and args.color:
+        color=args.color
     for char in text:
-        print(char, end='')
+        print(f"\033[{color}m{char}\033[0m", end='')
         time.sleep(delay)
-    print(Fore.RESET, end="")
