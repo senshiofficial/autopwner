6512c28a5bf3ec6468fdbd7372242e0a3c09f48a
Senshi <149282416+senshiofficial@users.noreply.github.com>
2023-11-25 03:49:30 +0900

    web support

--- p0rt_sn1p3r.py
+++ p0rt_sn1p3r.py
@@ -3,12 +3,13 @@ import time
 import json
 import argparse
 import intro
+import json
 target = "127.0.0.1"
 web_ports = [80, 8888, 8000, 8080, 443]
 app_ports = [20, 21, 22, 25, 53, 587, 3306, 3389]
 ports = []
 open_ports = []
-
+data = {}
 def argPort(ports):
     return_ports = []
     port_lenght = ports.split(",")
@@ -29,10 +30,12 @@ def argPort(ports):
     return list(set(return_ports))
 
 def main(args):
-    global ports, target
+    global ports, target, data
     intro.intro(args=None,text="\n ██▓███   ▒█████   ██▀███  ▄▄▄█████▓     ██████  ███▄    █  ██▓ ██▓███  ▓█████  ██▀███  \n▓██░  ██▒▒██▒  ██▒▓██ ▒ ██▒▓  ██▒ ▓▒   ▒██    ▒  ██ ▀█   █ ▓██▒▓██░  ██▒▓█   ▀ ▓██ ▒ ██▒\n▓██░ ██▓▒▒██░  ██▒▓██ ░▄█ ▒▒ ▓██░ ▒░   ░ ▓██▄   ▓██  ▀█ ██▒▒██▒▓██░ ██▓▒▒███   ▓██ ░▄█ ▒\n▒██▄█▓▒ ▒▒██   ██░▒██▀▀█▄  ░ ▓██▓ ░      ▒   ██▒▓██▒  ▐▌██▒░██░▒██▄█▓▒ ▒▒▓█  ▄ ▒██▀▀█▄  \n▒██▒ ░  ░░ ████▓▒░░██▓ ▒██▒  ▒██▒ ░    ▒██████▒▒▒██░   ▓██░░██░▒██▒ ░  ░░▒████▒░██▓ ▒██▒\n▒▓▒░ ░  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░  ▒ ░░      ▒ ▒▓▒ ▒ ░░ ▒░   ▒ ▒ ░▓  ▒▓▒░ ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░\n░▒ ░       ░ ▒ ▒░   ░▒ ░ ▒░    ░       ░ ░▒  ░ ░░ ░░   ░ ▒░ ▒ ░░▒ ░      ░ ░  ░  ░▒ ░ ▒░\n░░       ░ ░ ░ ▒    ░░   ░   ░         ░  ░  ░     ░   ░ ░  ▒ ░░░          ░     ░░   ░ \n             ░ ░     ░                       ░           ░  ░              ░  ░   ░     ")
     if args.target:
         target = args.target
+    else:
+        target = input("\ntarget IP \n$ ")
     if args.port:
         ports = argPort(args.port)
     elif args.app_ports:
@@ -42,7 +45,7 @@ def main(args):
     elif args.both_ports:
         ports = web_ports + app_ports
     else:
-        ports = web_ports
+        ports = list(map(int, input("target ports (split with ,)\n$ ").split(",")))
     nm = nmap.PortScanner()
     timeout = 3
     if args.no_vpn_prot:
@@ -52,7 +55,6 @@ def main(args):
     else:
         vpn_protected = isVpnProt()
     if vpn_protected:
-        print("Prot")
         for port in ports: # This is for the timeout
             raw_scan_result = nm.scan(target, arguments=f'-p {port} -sV')
             if raw_scan_result["nmap"]["scanstats"]["downhosts"] == "1":
@@ -65,22 +67,28 @@ def main(args):
         scan_result = nm.scan(target, arguments=f'-p {full_port} --min-rate 1000 -sV')
     print("\nPORT      NAME    SERVICE\n")
     for port in ports:
-            try:
-                if scan_result['scan'][target]['tcp'][port]['state'] == "closed":
-                    print("{} is closed".format(port))
-                else:
-                    product = scan_result['scan'][target]['tcp'][port]['product']
-                    version = scan_result['scan'][target]['tcp'][port]['version']
-                    name    = scan_result['scan'][target]['tcp'][port]['name']
-                    if version == "":
-                        version = "Not Found"
-                    port = str(port)
-                    if len(port) < 6:
-                        port += " "*(6-len(port))
-                    print("{}    {}    {} ({})".format(port, name, product, version))
-                    open_ports.append(port)
-            except Exception as e:
-                print(e)
+        try:
+            if scan_result['scan'][target]['tcp'][port]['state'] == "closed":
+                print("{} is closed".format(str(port) + " " * (10 - len(str(port)))))
+                data.setdefault("port_scan", {}).setdefault(target, {}).setdefault(port, {})["name"] = "closed"
+                continue
+
+            product = scan_result['scan'][target]['tcp'][port]['product']
+            version = scan_result['scan'][target]['tcp'][port]['version']
+            name = scan_result['scan'][target]['tcp'][port]['name']
+            if version == "":
+                version = "Not Found"
+            data.setdefault("port_scan", {}).setdefault(target, {}).setdefault(port, {})["name"] = name
+            data.setdefault("port_scan", {}).setdefault(target, {}).setdefault(port, {})["product"] = product
+            data.setdefault("port_scan", {}).setdefault(target, {}).setdefault(port, {})["version"] = version
+
+            print("{}    {}     {} ({})".format(port, name, product, version))
+        except Exception as e:
+            print(e)
+
+    with open("http/out.json", "w") as f:
+        json.dump(data, f, indent=4)
+
     if open_ports:
         return True
     return False
@@ -88,15 +96,13 @@ def main(args):
 def isVpnProt():
     print("\nport scanning protection is a mechanism wich stop the user from scanning ports. (for example ProtonVPN)\nif you have one the you should use any port scanners with a timeout or you get false results")
     while True:
-        vpn = input("Enter 'y' for Yes, 'n' for No, or '?' for Help\n$ ")
-        
-        if vpn in ['y', 'n', '?']:
-            if vpn == 'y':
-                return True
-            elif vpn == 'n':
-                return False
-            elif vpn == '?':
-                print("port scanning protection is a mechanism wich stop the user from scanning ports. (for example ProtonVPN)\nif you have one the you should use any port scanners with a timeout or you get false results")
-        else:
+        vpn = input("Enter [\033[44my\033[0m]es, [\033[41mn\033[0m]o, or '?' for Help\n$ ")
+        if vpn not in ['y', 'n', '?']:
             print("Invalid input. Please enter 'y', 'n', or '?'")
-#nmapScanWeb()
+            continue
+        if vpn == 'y':
+            return True
+        elif vpn == 'n':
+            return False
+        elif vpn == '?':
+            print("port scanning protection is a mechanism wich stop the user from scanning ports. (for example ProtonVPN)\nif you have one the you should use any port scanners with a timeout or you get false results")
